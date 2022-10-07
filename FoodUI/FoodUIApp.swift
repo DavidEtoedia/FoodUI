0154439033104c315cb16e1e6bbdcd80279c31e4
DavidEtoedia <etoedia@gmail.com>
2022-10-07 05:31:20 -0700

    Initial Commit

---
+++ FoodUI/FoodUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodUIApp.swift
+//  FoodUI
+//
+//  Created by Inyene Etoedia on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
